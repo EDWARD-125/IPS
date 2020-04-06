8a2ba477a788ea6ffeaac8826be675ebef82bd3b
EDWARDS pinto <edward.edpa@outlook.com>
2020-04-06 18:33:27 -0500

    Agregar archivos de proyecto.

---
+++ BLL/LiquidacionCuotaModeradoraService.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using DAL;
+namespace BLL
+{
+    public class LiquidacionCuotaModeradoraService
+    {
+        private LiquidacionCuotaRepository cuotaRepository;
+        public LiquidacionCuotaModeradoraService()
+        {
+            cuotaRepository = new LiquidacionCuotaRepository();
+        }
+        const double SALARIOMINIMO = 900000;
+
+        public void calcular(LiquidacionCuotaModeradora liquidacion)
+        {
+            LiquidarContributivo(liquidacion);
+            LiquidarSubsidiado(liquidacion);
+        }
+
+        private void LiquidarContributivo(LiquidacionCuotaModeradora liquidacion)
+        {
+            if (liquidacion.TipoAfiliacion.Equals("RC"))
+            {
+                if (liquidacion.SalarioDevengado < SALARIOMINIMO * 2)
+                {
+                    double Tarifa = 15;
+                    liquidacion.CuotaModeradora = liquidacion.ValorServicioHospitalizacion * Tarifa / 100;
+                    double TOPE = 250000;
+                    if (liquidacion.CuotaModeradora > TOPE)
+                    {
+                        liquidacion.CuotaModeradora = TOPE;
+                        liquidacion.Tope = "SI";
+                    }
+                    else
+                    {
+                        liquidacion.Tope = "NO";
+                    }
+                    liquidacion.Tarifa = Tarifa;
+                   
+                }
+
+                if (liquidacion.SalarioDevengado >= SALARIOMINIMO * 2 && liquidacion.SalarioDevengado <= SALARIOMINIMO * 5)
+                {
+                    double Tarifa = 20;
+                    liquidacion.CuotaModeradora = liquidacion.ValorServicioHospitalizacion * Tarifa / 100;
+                    double TOPE = 900000;
+                    if (liquidacion.CuotaModeradora > TOPE)
+                    {
+                        liquidacion.CuotaModeradora = TOPE;
+                        liquidacion.Tope = "SI";
+                    }
+                    else
+                    {
+                        liquidacion.Tope = "NO";
+                    }
+                    liquidacion.Tarifa = Tarifa;
+                }
+
+                if (liquidacion.SalarioDevengado > SALARIOMINIMO * 5)
+                {
+                    double Tarifa = 25;
+                    liquidacion.CuotaModeradora = liquidacion.ValorServicioHospitalizacion * Tarifa / 100;
+                    double TOPE = 1500000;
+                    if (liquidacion.CuotaModeradora > TOPE)
+                    {
+                        liquidacion.CuotaModeradora = TOPE;
+                        liquidacion.Tope = "SI";
+                    }
+                    else
+                    {
+                        liquidacion.Tope = "NO";
+                    }
+                    liquidacion.Tarifa = Tarifa;
+                }
+
+            }
+        }
+
+        private void LiquidarSubsidiado(LiquidacionCuotaModeradora liquidacion)
+        {
+            double Tarifa = 5;
+            if (liquidacion.TipoAfiliacion.Equals("RS"))
+            {
+                liquidacion.CuotaModeradora = liquidacion.ValorServicioHospitalizacion * Tarifa/100;
+                double Tope = 200000;
+                if (liquidacion.CuotaModeradora>Tope)
+                {
+                    liquidacion.CuotaModeradora = Tope;
+                    liquidacion.Tope = "SI";
+                }
+                else
+                {
+                    liquidacion.Tope = "NO";
+                }
+                liquidacion.Tarifa = Tarifa;
+            }
+        }
+
+        public string Guardar(LiquidacionCuotaModeradora liquidacion)
+        {
+            try
+            {
+                cuotaRepository.Guardar(liquidacion);
+                return "Los Datos han sido guardados satisfactoriamente";
+            }
+            catch (Exception e)
+            {
+
+                return "Error de Datos: " + e.Message;
+            }
+        }
+
+        public string Eliminar(string numeroLiquidacion)
+        {
+            try
+            {
+                if (cuotaRepository.Buscar(numeroLiquidacion)!=null)
+                {
+                    cuotaRepository.Eliminar(numeroLiquidacion);
+                    return $"se elimino la liquidacion numero: {numeroLiquidacion} correctamente";
+                }
+                return $"El numero de liquidacion no esta registrado en el sistema";
+            }
+            catch (Exception e)
+            {
+                return $"ERROR" + e.Message;
+            }
+        }
+
+        public RespuestaConsulta ConsultarConsultaGeneral()
+        {
+            RespuestaConsulta respuesta = new RespuestaConsulta();
+            try
+            {
+                respuesta.Error = false;
+                respuesta.liquidaciones = cuotaRepository.Consultar();
+                if (respuesta.liquidaciones!=null)
+                {
+                    respuesta.Mensaje = "LISTADO DE LIQUIDACIONES";
+                }
+                else
+                {
+                    respuesta.Mensaje = "NO HAY DATOS";
+                }
+            }
+            catch (Exception e)
+            {
+
+                respuesta.Error = true;
+                respuesta.Mensaje = $"ERROR" + e.Message;
+            }
+            return respuesta;
+        }
+
+        public string Modificar(LiquidacionCuotaModeradora liquidacion)
+        {
+            try
+            {
+                if (cuotaRepository.Buscar(liquidacion.NumeroLiquidacion)!=null)
+                {
+                    cuotaRepository.Modificar(liquidacion);
+                    return $"la liquidacion numero: {liquidacion.NumeroLiquidacion} ha sido modificada con exito!";
+
+                }
+                return $"El numero de liquidacion: {liquidacion.NumeroLiquidacion} no se encuentra registrada por favor verifique los datos";
+
+            }
+            catch (Exception e)
+            {
+
+                return "Error de datos" + e.Message;
+            }
+        }
+
+    }
+
+    public class RespuestaBusqueda
+    {
+        public string Mensaje { get; set; }
+        public LiquidacionCuotaModeradora liquidacion { get; set; }
+        public bool Error { get; set; }
+    }
+
+    public class RespuestaConsulta
+    {
+        public string Mensaje { get; set; }
+        public List<LiquidacionCuotaModeradora> liquidaciones { get; set; }
+        public bool Error { get; set; }
+    }
+}
---
+++ IPS/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using BLL;
+
+namespace IPS
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            LiquidacionCuotaModeradoraService service = new LiquidacionCuotaModeradoraService();
+             Guardar(service);
+            ////   Eliminar();
+              ConsultaGeneral(service);
+           // Modificar(service);
+        }
+
+        private static void Guardar(LiquidacionCuotaModeradoraService service)
+        {
+            string numeroLiquidacion, identificacion, nombre, tipoAfiliacion;
+            double salarioDevengado, valorServicioHospitalizacion;
+            Console.Write("Digite numero de liquidacion: ");
+            numeroLiquidacion = Console.ReadLine();
+
+            Console.Write("Digite identificacion: ");
+            identificacion = Console.ReadLine();
+
+            Console.Write("Digite nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Digite tipo de afiliacion: ");
+            tipoAfiliacion = Console.ReadLine();
+
+            Console.Write("Digite salario devengado: ");
+            salarioDevengado = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite valor servicio de hospitalizacion: ");
+            valorServicioHospitalizacion = double.Parse(Console.ReadLine());
+
+            LiquidacionCuotaModeradora liquidacion = new LiquidacionCuotaModeradora()
+            {
+                NumeroLiquidacion = numeroLiquidacion,
+                Identificacion = identificacion,
+                Nombre = nombre,
+                TipoAfiliacion = tipoAfiliacion,
+                SalarioDevengado = salarioDevengado,
+                ValorServicioHospitalizacion = valorServicioHospitalizacion
+            };
+
+            service.calcular(liquidacion);
+            string mensaje=service.Guardar(liquidacion);
+            Console.Write(mensaje);
+            Console.WriteLine(liquidacion.ToString());
+            Console.ReadKey();
+        }
+
+        public static void Eliminar()
+        {
+            Console.Clear();
+            Console.WriteLine("ELIMINAR");
+            LiquidacionCuotaModeradora liquidacion = new LiquidacionCuotaModeradora();
+            LiquidacionCuotaModeradoraService service = new LiquidacionCuotaModeradoraService();
+            Console.Write("Digite numero de liquidacion a eliminar: ");
+            string numeroLiquidacion;
+            numeroLiquidacion = Console.ReadLine();
+            string mensaje = service.Eliminar(numeroLiquidacion);
+            Console.WriteLine(mensaje);
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void ConsultaGeneral(LiquidacionCuotaModeradoraService service)
+        {
+            Console.Clear();
+            RespuestaConsulta respuestaConsulta = service.ConsultarConsultaGeneral();
+            Console.WriteLine(respuestaConsulta.Mensaje);
+            if (!respuestaConsulta.Error)
+            {
+                foreach (var item in respuestaConsulta.liquidaciones)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        //        private static void Modificar(LiquidacionCuotaModeradoraService service)
+        //        {
+        //            // Console.Clear();
+        //            LiquidacionCuotaModeradora liquidacion1 = new LiquidacionCuotaModeradora()
+        //;
+        //            string numeroLiquidacion, identificacion, nombre, tipoAfiliacion, tope;
+        //            double valorServicioHospitalizacion, salarioDevengado, cuotaModeradora, tarifa;
+        //            nombre=liquidacion1.Nombre.ToString();
+        //            identificacion = liquidacion1.Identificacion.ToString();
+        //            tipoAfiliacion = liquidacion1.TipoAfiliacion.ToString();
+        //            salarioDevengado = double.Parse(liquidacion1.SalarioDevengado.ToString());
+        //            cuotaModeradora = double.Parse(liquidacion1.CuotaModeradora.ToString());
+        //            tarifa = double.Parse(liquidacion1.Tarifa.ToString());
+        //            tope = liquidacion1.Tope.ToString();
+        //            Console.WriteLine("\t\t\tMODIFICAR UNA LIQUIDACION");
+        //            Console.Write("\t\t\tDigite numero de liquidacion: ");
+        //            numeroLiquidacion = Console.ReadLine();
+        //            Console.Write("\t\t\tDigite valor: ");
+        //            valorServicioHospitalizacion = double.Parse(Console.ReadLine());
+
+        //            LiquidacionCuotaModeradora liquidacion = new LiquidacionCuotaModeradora()
+        //            {
+        //                NumeroLiquidacion = numeroLiquidacion.ToString(),
+        //                Identificacion = identificacion.ToString(),
+        //                Nombre=nombre.ToString(),
+        //                TipoAfiliacion=tipoAfiliacion.ToString(),
+        //                SalarioDevengado=double.Parse(salarioDevengado.ToString()),
+        //                ValorServicioHospitalizacion = double.Parse(valorServicioHospitalizacion.ToString()),
+        //                CuotaModeradora=double.Parse(cuotaModeradora.ToString()),
+        //                Tarifa=double.Parse(tarifa.ToString()),
+        //                Tope=tope.ToString()
+        //            };
+        //            service.calcular(liquidacion);
+        //            string mensaje = service.Modificar(liquidacion);
+        //            Console.WriteLine(mensaje);
+        //            Console.ReadKey();
+        //            Console.Clear();
+        //        }
+
+        private static void Modificar(LiquidacionCuotaModeradoraService service)
+        {
+            // Console.Clear();
+           
+            Console.WriteLine("\t\t\tMODIFICAR UNA LIQUIDACION");
+            Console.Write("\t\t\tDigite numero de liquidacion: ");
+            string numeroLiquidacion;
+            double valorServicioHospitalizacion;
+            numeroLiquidacion = Console.ReadLine();
+            Console.Write("\t\t\tDigite valor: ");
+            valorServicioHospitalizacion = double.Parse(Console.ReadLine());
+
+            LiquidacionCuotaModeradora liquidacion = new LiquidacionCuotaModeradora()
+            {
+                NumeroLiquidacion = numeroLiquidacion,
+                
+                ValorServicioHospitalizacion = valorServicioHospitalizacion
+               
+            };
+            service.calcular(liquidacion);
+            string mensaje = service.Modificar(liquidacion);
+            Console.WriteLine(mensaje);
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+}
